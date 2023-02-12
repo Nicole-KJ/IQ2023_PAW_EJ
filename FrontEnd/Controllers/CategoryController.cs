49c3633f1d3e68f946c77909b58e4cc546450136
Keren Nicole Jimenez Fernandez <72894324+Nicole-KJ@users.noreply.github.com>
2023-02-11 22:32:06 -0600

    FrontEnd: CategoryHelper, CategoryController & basic views completed

---
+++ FrontEnd/Controllers/CategoryController.cs
@@ -0,0 +1,113 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        CategoryHelper categoryHelper;
+
+        // GET: CategoryController
+        public ActionResult Index()
+        {
+
+            categoryHelper = new CategoryHelper();
+            List<CategoryViewModel> lista = categoryHelper.GetAll();
+
+            return View(lista);
+        }
+
+        // GET: CategoryController/Details/5
+        public ActionResult Details(int id)
+        {
+            categoryHelper = new CategoryHelper();
+            CategoryViewModel category = categoryHelper.Get(id);
+
+            return View(category);
+        }
+
+        // GET: CategoryController/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: CategoryController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CategoryViewModel category)
+        {
+            try
+            {
+                categoryHelper = new CategoryHelper();
+                category = categoryHelper.Create(category);
+
+                return RedirectToAction("Details", new { id = category.CategoryId });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoryController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            categoryHelper = new CategoryHelper();
+            CategoryViewModel category = categoryHelper.Get(id);
+
+            return View(category);
+        }
+
+        // POST: CategoryController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CategoryViewModel category)
+        {
+            try
+            {
+                CategoryHelper categoryHelper = new CategoryHelper();
+                category = categoryHelper.Edit(category);
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoryController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            categoryHelper = new CategoryHelper();
+            CategoryViewModel category = categoryHelper.Get(id);
+
+            return View(category);
+        }
+
+        // POST: CategoryController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(CategoryViewModel category)
+        {
+            try
+            {
+                categoryHelper = new CategoryHelper();
+                categoryHelper.Delete(category.CategoryId);
+
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FrontEnd/Helpers/CategoryHelper.cs
@@ -0,0 +1,95 @@
+﻿using FrontEnd.Models;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Helpers
+{
+    public class CategoryHelper
+    {
+        private ServiceRepository ServiceRepository;
+        public CategoryHelper()
+        {
+            ServiceRepository = new ServiceRepository();
+        }
+
+        public List<CategoryViewModel> GetAll()
+        {
+            List<CategoryViewModel> lista;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/category/");
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            lista = JsonConvert.DeserializeObject<List<CategoryViewModel>>(content);
+
+
+            return lista;
+        }
+
+        public CategoryViewModel Get(int id)
+        {
+            CategoryViewModel Category;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.GetResponse("api/category/" + id.ToString());
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            Category = JsonConvert.DeserializeObject<CategoryViewModel>(content);
+
+
+
+            return Category;
+        }
+
+
+        public CategoryViewModel Create(CategoryViewModel category)
+        {
+
+
+            CategoryViewModel Category;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.PostResponse("api/category/", category);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            Category = JsonConvert.DeserializeObject<CategoryViewModel>(content);
+
+
+
+            return Category;
+        }
+
+
+
+
+        public CategoryViewModel Edit(CategoryViewModel category)
+        {
+
+
+            CategoryViewModel Category;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.PutResponse("api/category/", category);
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            Category = JsonConvert.DeserializeObject<CategoryViewModel>(content);
+
+
+
+            return Category;
+        }
+
+
+
+        public CategoryViewModel Delete(int id)
+        {
+
+
+            CategoryViewModel Category;
+
+
+            HttpResponseMessage responseMessage = ServiceRepository.DeleteResponse("api/category/" + id.ToString());
+            var content = responseMessage.Content.ReadAsStringAsync().Result;
+            Category = JsonConvert.DeserializeObject<CategoryViewModel>(content);
+
+
+
+            return Category;
+        }
+    }
+}
